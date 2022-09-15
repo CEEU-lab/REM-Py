8840e07c845fc4c48ed09589c3b9ad1fff41592b
Federico <fcatalano@unsam.edu.ar>
2022-09-15 11:15:12 -0300

    Actualiza modulo de modelos
    Leer modelo entrenado desde recurso externo

--- REM/models.py
+++ REM/models.py
@@ -44,29 +44,33 @@ def find_min_rmse(nf, x, data_dmatrix):
 
     return cv_results['test-rmse-mean'].min()
 
-def test_cv_parameters(k, hyper_parameters, nf, data_dmatrix):
+def test_cv_parameters(k, bp, ap, nf, data_dmatrix):
     title = "Optimizing {}".format(k)
+    print('')
     print(title)
     print('*'*len(title))
     lst = []
 
-    for v in hyper_parameters[k]:
+    for v in ap[k]:
         print(v)
-        # TODO: parametrizar el array de base.
-        # este es la mejor linea de base obtenida antes de la validacion cruzada
         if k == 'colsample_bytree':
-            array = [v, 0.1, 5, 10, 45]
+            array = [v, bp[1], bp[2], bp[3], bp[4]]
+
         elif k == 'learning_rate':
-            array=[0.3, v, 5, 10, 45]
+            array=[bp[0], v, bp[2], bp[3], bp[4]]
+
         elif k == 'max_depth':
-            array=[0.3, 0.1, v, 10, 45]
+            array=[bp[0], bp[1], v, bp[3], bp[4]]
+
         elif k == 'alpha':
-            array=[0.3, 0.1, 5, v, 45]
+            array=[bp[0], bp[1], bp[2], v, bp[4]]
+
         elif k == 'num_boost_round':
-            array=[0.3, 0.1, 5, 10, v]
+            array=[bp[0], bp[1], bp[2], bp[3], v]
+
         else:
             pass
-
+        print(array)
         min_rmse = find_min_rmse(nf, array, data_dmatrix)
         lst.append(min_rmse)
 
@@ -79,8 +83,6 @@ def print_scores(y_test, preds):
     r2 = np.sqrt(r2_score(y_test, preds))
     print("R_Squared Score : %f" % (r2))
 
-def int_block_groups(x):
-    return int(x[1:])
 
 def plot_predicted_vs_test(x_ax, y_test, preds, predicted_name):
     '''
@@ -89,16 +91,16 @@ def plot_predicted_vs_test(x_ax, y_test, preds, predicted_name):
     plt.figure(figsize=(14, 5), dpi=80)
     plt.plot(x_ax, y_test, linewidth=0.75, label="test")
     plt.plot(x_ax, preds, '--', linewidth=0.5, color='red', label="predicted",)
-    plt.title("{} - Test and Predicted data".format(predicted_name))
+    plt.title("Parcels {} - Test and Predicted data".format(predicted_name))
     plt.legend()
     plt.xlabel('Parcel idx')
     plt.ylabel('{} (USD)'.format(predicted_name))
     plt.grid(True)
     plt.show();
 
-def plot_cross_val(alternatives,results,parameter, color):
+def plot_min_rmse(alternatives,results,parameter, color):
     '''
-    Plots hyperparameter optimization along Cross Validation Test.
+    Plots RMSE change with different parameter values.
     '''
     plt.figure(figsize=(12, 4))
     plt.plot(alternatives, results, color)
@@ -124,25 +126,9 @@ def optimal_hyperparam(dict1, dict2):
     idx = optimal.index[0]
     return optimal[idx]
 
-def grid_search_optimization(X_train,y_train):
+def grid_search_optimization(params, X_train,y_train):
     # Gradient Boosting
-    params = {
-              'n_estimators':[250, 500, 1000],
-              'max_depth':[3,5],
-              'learning_rate':np.linspace(0.001,0.1,4),
-              'gamma':np.linspace(0,1,5),
-              'min_child_weight':np.linspace(1,5,3),
-              'subsample':np.linspace(0.3,0.9,3),
-              'colsample_bytree':np.linspace(0.3,0.9,3),
-              'reg_alpha':[0,.01,.1,.5,1],
-              'reg_lambda':[0,.01,.1,.5,1],
-              'scale_pos_weight':np.linspace(1,5,3)
-              }
-
-
     xgb_estimator = xgb.XGBRegressor(seed=42)
-
-
     gsearch = GridSearchCV(estimator=xgb_estimator,param_grid=params,
                            scoring='neg_root_mean_squared_error',n_jobs=-1,
                            refit='neg_root_mean_squared_error',cv=5,verbose=11)
@@ -152,16 +138,3 @@ def grid_search_optimization(X_train,y_train):
     print(f'Best Parameters: {gsearch.best_params_}')
     print(f'Best Score: {gsearch.best_score_}')
     return gsearch
-
-
-def xgb_regressor(to_predict_df, expvars, best_params):
-    xg_reg = xgb.XGBRegressor(objective ='reg:squarederror',
-                              colsample_bytree=best_params['colsample_bytree'],
-                              learning_rate=best_params['learning_rate'],
-                              max_depth=best_params['max_depth'],
-                              alpha=best_params['alpha'],
-                              n_estimators=best_params['num_boost_round'],
-                              subsample=0.45, # TODO: Parametrizar
-                              gamma=1000000) # TODO: Parametrizar
-    predicted_vals = xg_reg.predict(to_predict[expvars])
-    return xpredicted_vals
